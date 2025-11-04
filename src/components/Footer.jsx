847b906322fe28d8cbe8a02edf437a8ec6df0677
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-11-04 16:37:41 +1100

    footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,41 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or other retailer near you. Or
+          call 1-800-MY-APPLE.
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>
+          Copyright © 2025 Apple Inc. All rights reserved. Developed by{" "}
+          <a
+            href="http://www.arthurlaroya.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary"
+          >
+            Arthur Laroya
+          </a>{" "}
+        </p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
